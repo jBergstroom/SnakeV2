ac6910a7e1904ac22993b3ff68bb756828b964c1
Jakob Segerslätt <segerslatt@gmail.com>
2016-03-16 11:31:53 +0100

    snake class!

---
+++ SnakeV2/Snake.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeV2
+{
+    class Snake
+    {
+        public int HeadX { get; set; }
+        public int HeadY { get; set; }
+    }
+}
