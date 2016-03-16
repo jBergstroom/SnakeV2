a6e406657e4e884f03ac432a3f6a34eda8584ceb
Jakob Segerslätt <segerslatt@gmail.com>
2016-03-16 13:02:32 +0100

    13.02

--- SnakeV2/Coordinate.cs
+++ SnakeV2/Coordinate.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace SnakeV2
 {
-    class Coordinate
+    class Point
     {
-        public int Xcoordinate { get; set; }
-        public int Ycoordinate { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsEmpty { get; set; }
     }
 }
