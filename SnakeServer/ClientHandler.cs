f265f900d18dea76f074dac319aa7e5b45b82857
Viktor Karlsson <viktorkarlsson@live.com>
2016-03-17 12:57:24 +0100

    Lölz

--- SnakeServer/ClientHandler.cs
+++ SnakeServer/ClientHandler.cs
@@ -32,7 +32,8 @@ namespace SnakeServer
                     message = new BinaryReader(n).ReadString();
                     message.Trim();
                     nameOccupied = myServer.clientList.Select(x => x.name).Contains(message);
-                    if (message != "" && message.Substring(0, 4) == "name")
+                    string partName = message.Substring(0, 4);
+                    if (message != "" && partName == "name")
                     {
                         if (nameOccupied)
                         {
@@ -41,16 +42,18 @@ namespace SnakeServer
                         else
                         {
                             myServer.SingleBroadcast(this, $"0;Name {message} is ok");
-                            name = message;
+                            string restname = message.Substring(4);
+                            name = restname;
                             //myServer.Broadcast($"{message} has join the gaming worlds");
                         }
                     }
                 } while (nameOccupied);
 
+                myServer.Broadcast("Waiting for other player;");
                 while (!myServer.GejmÅn)
                 {
-                    myServer.Broadcast("Waiting for other player;");
-                    Thread.Sleep(5);
+                    Console.WriteLine(name + " is waiting for game to start");
+                    Thread.Sleep(200);
                 }
                 while (myServer.GejmÅn && message != "ded")
                 {
